9f231af75bd9ea25ff294ce4cb3329acdc1de3f7
Denis Chertenko <denis.chertenko@mail.ru>
2020-04-19 18:46:59 +0300

    feat(routes) сделать все типы запросов

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const todoRoutes = require('./routes/todo');
 const path = require('path');
 
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(express.json());
 app.use('/api/todo', todoRoutes);
 
 app.use((req, res, next) => {
--- routes/todo.js
+++ routes/todo.js
@@ -1,24 +1,61 @@
 const {Router} = require('express');
+const Todo = require('../models/Todo');
 const router = Router();
 
 // получение списка
-router.get('/', (req, res) => {
-    res.json({a: 1});
+router.get('/', async (req, res) => {
+    try {
+        const todos = await Todo.findAll();
+        res.status(200).json(todos)
+    } catch (e) {
+        res.status(500).json({
+            message: 'Server error'
+        })
+    }
 });
 
 // создание новой задачи
-router.post('/', (req, res) => {
-    res.json({a: 1});
+router.post('/', async (req, res) => {
+    try {
+        const todo = await Todo.create({
+            title: req.body.title,
+            done: false
+        });
+        res.status(201).json({todo});
+    } catch (e) {
+        console.log(e);
+        res.status(500).json({
+            message: 'Server error'
+        })
+    }
 });
 
 // изменение задачи
-router.put('/:id', (req, res) => {
-    res.json({a: 1});
+router.put('/:id', async (req, res) => {
+    try {
+        const todo = await Todo.findByPk(+req.params.id);
+        todo.done = req.body.done;
+        await todo.save();
+        res.status(200).json({todo});
+    } catch (e) {
+        console.log(e)
+    }
 });
 
 // удаление задачи
-router.delete('/:id', (req, res) => {
-    res.json({a: 1});
+router.delete('/:id', async (req, res) => {
+    try {
+        const todos = await Todo.findAll({
+            where: {
+                id: +req.params.id
+            }
+        });
+        const todo = todos[0];
+        await todo.destroy();
+        res.status(204).json({});
+    } catch (e) {
+        console.log(e)
+    }
 });
 
 module.exports = router;
